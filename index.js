5c60729d17b7333ae55838e51d5ef64b6a6f340b
Adarsh singh <adarshsingh0930@gmail.com>
2023-12-30 15:44:13 +0530

    creates project outline

---
+++ index.js
@@ -0,0 +1,8 @@
+// read location as a command line arguments
+const location = (process.argv[2]);
+
+// get latitude & longitude of the location using geocoding APIs
+
+// use coordinates to get weather details using weather APIs
+
+// display weather details as standard output
